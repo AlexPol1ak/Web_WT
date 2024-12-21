2446505a5e1d03b09df92456905bd43e667584e6
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2024-12-22 01:34:27 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Poliak_UI_WT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
