74950fe3a5e09c911d230a91d60dfde982aae75c
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2024-12-23 18:53:21 +0300

    The user cart has been moved to a view component and is placed in the _UserInfoPartial view State: Lb 2 Final

---
+++ ViewComponents/CartViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Poliak_UI_WT.ViewComponents
+{
+    public class CartViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    } 
+}
