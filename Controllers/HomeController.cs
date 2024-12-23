e12f3007aa2804359b6c304aeec478ddbd44696d
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2024-12-23 17:15:42 +0300

    Added links using tag-helpers. Displayed test list using SelectList. Status: Lb 2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Poliak_UI_WT.Models;
 
 namespace Poliak_UI_WT.Controllers
 {
@@ -6,6 +8,17 @@ namespace Poliak_UI_WT.Controllers
     {
         public IActionResult Index()
         {
+            ViewData.Add("TitleLab", "Лабораторная работа 2");
+
+            var items = new List<ListDemo>
+        {
+            new ListDemo { Id = 1, Name = "Item 1" },
+            new ListDemo { Id = 2, Name = "Item 2" },
+            new ListDemo { Id = 3, Name = "Item 3" }
+        };
+            var selectList = new SelectList(items, "Id", "Name");
+            ViewBag.Items = selectList;
+
             return View();
         }
     }
---
+++ Models/ListDemo.cs
@@ -0,0 +1,8 @@
+﻿namespace Poliak_UI_WT.Models
+{
+    public class ListDemo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
